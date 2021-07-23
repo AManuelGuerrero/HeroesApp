5fe3ca043b34284719385cad757c02daf78d8c5e
Antonio Manuel Guerrero Trujillo <antonio.guerrero@atmira.com>
2021-07-23 09:42:01 +0200

    first commit

---
+++ src/app/heroes/heroes.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HeroesRoutigModule } from './heroes-routig.module';
+
+import { AgregarComponent } from './pages/agregar/agregar.component';
+import { BuscarComponent } from './pages/buscar/buscar.component';
+import { HeroeComponent } from './pages/heroe/heroe.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ListadoComponent } from './pages/listado/listado.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    AgregarComponent,
+    BuscarComponent,
+    HeroeComponent,
+    HomeComponent,
+    ListadoComponent
+  ],
+  imports: [
+    CommonModule,
+    HeroesRoutigModule
+  ]
+})
+export class HeroesModule { }
